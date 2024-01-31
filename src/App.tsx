b222e93b0809a3a35ca06a8f63a406a2db16ed94
John Ericksen <john.paul.ericksen@gmail.com>
2024-01-31 09:32:33 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import PomodoroTimer from './components/PomodoroTimer'
+
+function App() {
+  
+  return (
+    <>
+      <PomodoroTimer/>
+    </>
+  )
+}
+
+export default App
