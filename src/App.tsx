681d32080afd4b34ac49e28ed2365b838f51d2ec
John Ericksen <john.paul.ericksen@gmail.com>
2024-02-17 19:46:13 -0500

    rename Main component to avoid naming confusion

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-import Main from './components/Main';
+import Content from './components/Content';
 
 function App() {
   return (
     <>
-      <Main/>
+      <Content/>
     </>
   )
 }
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -2,11 +2,10 @@ import Settings from "./Settings";
 import Timer from "./Timer";
 import {useState } from "react";
 
-export default function Main() {
+export default function Content() {
   const [timerValues, setTimerValues] = useState([1500 * 1000, 300 * 1000, 900 * 1000]);
   const [defaultworkCycles, setDefaultWorkCycles] = useState(3); //the amount of work -> short break cycles before a long break is given
   
-
   return (
     <>
       <Timer timerValues={timerValues} setTimerValues={setTimerValues} defaultWorkCycles={defaultworkCycles}/>
