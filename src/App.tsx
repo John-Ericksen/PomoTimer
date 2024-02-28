a3afe12a4fdae7328f6a14aa911023ef437f9662
John-Ericksen <105228679+John-Ericksen@users.noreply.github.com>
2024-02-28 16:09:37 -0500

    begin adding joyui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
 import Content from './components/Content/Content';
+import { CssVarsProvider } from '@mui/joy/styles';
+import { CssBaseline } from '@mui/joy';
 
 function App() {
   return (
-    <>
+    <CssVarsProvider defaultMode='dark'>
+      <CssBaseline/>
       <Content/>
-    </>
+    </CssVarsProvider>
   )
 }
 export default App
